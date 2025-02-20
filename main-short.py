dc3fba72c4643be8fc9c206a9ced650c961de6d1
Nayana-S-2004 <snayana877@gmail.com>
2025-02-20 18:57:50 +0530

    initial commit

---
+++ main-short.py
@@ -0,0 +1,33 @@
+import random
+
+print("enter r for rock\nenter p for paper\nenter s for scissor")
+
+computer=random.choice([-1,0,1])
+youstr=input("Enter your choice: ")
+youDict={"r":1,"p":-1,"s":0}
+reverseDict={1:"rock",-1:"paper",0:"scissor"}
+
+you=youDict[youstr]
+
+print(f"you chose {reverseDict[you]}\ncomputer chose {reverseDict[computer]}")
+
+if(computer==you):
+    print("It's a draw")
+
+else:
+    # if(computer==-1 and you==0):(computer-you=-1)
+    #     print("You win!")
+    # elif(computer==1 and you==0):(computer-you=1)
+    #     print("You lose!")
+    # elif(computer==0 and you==-1):(computer-you=1)
+    #     print("You lose!")
+    # elif(computer==0 and you==1):(computer-you=-1)
+    #     print("you win!")
+    # elif(computer==-1 and you==1):(computer-you=-2)
+    #     print("you win!")
+    # elif(computer==1 and you==-1):(computer-you=2)
+    #     print("Yoy lose!")h
+    if(computer-you==-1 or computer-you==-2):
+        print("You Win!")
+    else:
+        print("You lose!")
---
+++ main.py
@@ -0,0 +1,31 @@
+import random
+
+print("enter r for rock\nenter p for paper\nenter s for scissor")
+
+computer=random.choice([-1,0,1])
+youstr=input("Enter your choice: ")
+youDict={"r":1,"p":-1,"s":0}
+reverseDict={1:"rock",-1:"paper",0:"scissor"}
+
+you=youDict[youstr]
+
+print(f"you chose {reverseDict[you]}\ncomputer chose {reverseDict[computer]}")
+
+if(computer==you):
+    print("It's a draw")
+
+else:
+    if(computer==-1 and you==0):
+        print("You win!")
+    elif(computer==1 and you==0):
+        print("Computr win!")
+    elif(computer==0 and you==-1):
+        print("Computer win!")
+    elif(computer==0 and you==1):
+        print("you win!")
+    elif(computer==-1 and you==1):
+        print("you win!")
+    elif(computer==1 and you==-1):
+        print("Computer win!")
+    else:
+        print("Something went wrong!")
